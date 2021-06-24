c293d9ae3ae975843a3a03eac618ba83dc4c2fcb
eliss94 <eliss94@seznam.cz>
2021-06-24 08:06:43 +0200

    Add project files.

---
+++ PrihlaseniEmailTest/EmailTest.feature
@@ -0,0 +1,31 @@
+﻿Feature: EmailTest
+	Test prihlaseni do emailu na seznamu 
+
+@mytag
+Scenario: Prihlaseni do emailu 
+	Given jsem na webu seznam.cz
+	Then jako jmeno zadam <jmeno> 
+	Then jako heslo zadam <heslo>
+	Then jako domenu zadam <domena>
+	Then kliknu na prihlasit
+	Then zkontroluji uspesnost prihlaseni
+	Then zavru prohlizec
+
+	Examples: 
+	| jmeno     | heslo   | domena   |
+	| example11 | PpFf123 | email.cz |
+
+Scenario: Spatne prihlaseni do emailu
+	Given jsem na webu seznam.cz
+	Then jako jmeno zadam <jmeno>
+	Then jako domenu zadam <domena>
+	Then jako heslo zadam <heslo>
+	Then kliknu na prihlasit
+	Then zkontroluji neuspesnost prihlaseni
+	Then zavru prohlizec
+	
+	Examples: 
+	| jmeno  | domena    |heslo    |
+	|        | seznam.cz |jouza356 |
+	| walter | email.cz  |         |
+
---
+++ PrihlaseniEmailTest/EmailTestSteps.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace PrihlaseniEmailTest
+{
+    [Binding]
+    public class EmailTestSteps
+    {
+        IWebDriver driver;
+        string uvodnistranka = "https://www.seznam.cz/";
+        string cilovaAdresa = "https://email.seznam.cz/?hp=";
+
+        [Given(@"jsem na webu seznam\.cz")]
+        public void GivenJsemNaWebuSeznam_Cz()
+        {
+            driver = new FirefoxDriver();
+            driver.Url = uvodnistranka;
+            driver.Manage().Window.Maximize();
+            System.Threading.Thread.Sleep(2000);
+        }
+
+        [Then(@"jako jmeno zadam (.*)")]
+        public void ThenZadamPrihlasovaciJmeno(string jmeno)
+        {
+            var prihlasJmeno = driver.FindElement(By.Name("username"));
+            prihlasJmeno.SendKeys(jmeno);
+
+        }
+        [Then(@"jako heslo zadam (.*)")]
+        public void ThenJakoHesloZadam(string heslo)
+        {
+            var elementHeslo = driver.FindElement(By.Name("password"));
+            elementHeslo.SendKeys(heslo);
+        }
+
+
+        [Then(@"jako domenu zadam (.*)")]
+        public void ThenZadamHesloAKliknuNaPrihlasit(string domena)
+        {
+            var elementDomena = driver.FindElement(By.Name("domain"));
+            var vybranaDomena = new SelectElement(elementDomena);
+            vybranaDomena.SelectByValue(domena);
+
+
+        }
+
+        [Then(@"kliknu na prihlasit")]
+        public void ThenKliknuNaPrihlasit()
+        {
+            var tlacitkoPrihlas = driver.FindElement(By.CssSelector(".input-w-button__button"));
+            tlacitkoPrihlas.Click();
+            System.Threading.Thread.Sleep(2000);
+        }
+
+
+        [Then(@"zkontroluji uspesnost prihlaseni")]
+        public void ThenZkontrolujiUspesnostPrihlaseni()
+        {
+            var aktualniAdresa = driver.Url;
+            Assert.That(cilovaAdresa.Contains(aktualniAdresa), Is.True);
+        }
+        [Then(@"zkontroluji neuspesnost prihlaseni")]
+        public void ThenZkontrolujiNeuspesnostPrihlaseni()
+        {
+            var aktualniAdresa = driver.Url;
+            Assert.That(!cilovaAdresa.Contains(aktualniAdresa), Is.True);
+        }
+
+
+        [Then(@"zavru prohlizec")]
+        public void ThenZavruProhlizec()
+        {
+            driver.Quit();
+        }
+    }
+}
